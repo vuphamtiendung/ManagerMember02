ec9db0e422139d834fe4a618a8bef98469bdfc5b
DungVPT <vuphamtiendung@gmail.com>
2023-03-29 01:05:06 +0700

    commit successfully version 0.6

--- MemberManager/DataTransferObject/DTO_Member.cs
+++ MemberManager/DataTransferObject/DTO_Member.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace DataTransferObject
 {
-    internal class DTO_Member
+    public class DTO_Member
     {
+        private int _memberId;
+        private string _memberFullName;
+        private int _memberPhone;
+        private string _memberEmail;
+
+        public int MemberId { get { return _memberId; } set { _memberId = value; } }
+        public string MemberFullName { get { return _memberFullName; } set { _memberFullName = value; } }
+        public int MemberPhone { get { return _memberPhone; } set { _memberPhone = value; } }
+        public string MemberEmail { get { return _memberEmail; } set { _memberEmail = value;} }
+
+        public DTO_Member() { }
+        public DTO_Member(int memberId, string memberFullName, int memberPhone, string memberEmail)
+        {
+            _memberId = memberId;
+            _memberFullName = memberFullName;
+            _memberPhone = memberPhone;
+            _memberEmail = memberEmail;
+        }
     }
 }
