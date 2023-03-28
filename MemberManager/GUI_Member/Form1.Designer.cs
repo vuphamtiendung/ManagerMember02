2eeec154a5af35b7b16b0eeb40603de25f2177d3
DungVPT <vuphamtiendung@gmail.com>
2023-03-29 00:52:43 +0700

    Commit successfully version 0.3

--- MemberManager/GUI_Member/Form1.Designer.cs
+++ MemberManager/GUI_Member/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(825, 364);
+            this.ClientSize = new System.Drawing.Size(830, 366);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
